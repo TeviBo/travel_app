2b5f573cddde35bfc119fae990b1673ac0a75351
Esteban Bobbiesi <e.bobbiesi@gmail.com>
2023-04-20 19:01:49 -0300

    Refactored db variables

--- app/travel_blog/settings/local.py
+++ app/travel_blog/settings/local.py
@@ -8,11 +8,11 @@ ALLOWED_HOSTS = []
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": get_secret("DB_NAME"),
-        "USER": get_secret("USER"),
-        "PASSWORD": get_secret("PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
