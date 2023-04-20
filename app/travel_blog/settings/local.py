2cee3cb2bd00a80a3d85b0fd7005a44652396fda
Esteban Bobbiesi <e.bobbiesi@gmail.com>
2023-04-20 19:48:42 -0300

    Removed older environment variables use. Implemented os.environ.get() method

--- app/travel_blog/settings/local.py
+++ app/travel_blog/settings/local.py
@@ -4,13 +4,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("USER"),
-        "PASSWORD": os.environ.get("PASSWORD"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
     }
