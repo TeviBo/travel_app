d66581c9f5d919f8051d65a0d4f61759f530950e
Esteban Bobbiesi <e.bobbiesi@gmail.com>
2023-04-21 18:02:19 -0300

    Added test for user creation with short password

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -60,3 +60,23 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_short_password(self):
+        """
+        Test creating an user with short password throws an error.
+        """
+        payload = {
+            'email': fake.email(),
+            'password': 'Ab123',
+            'full_name': f'{fake.first_name()} {fake.last_name()}',
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+
+        self.assertFalse(user_exists)
