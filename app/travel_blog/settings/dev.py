8a2c0318ab5a04bb4ddc32196c2022d8eddf4bb0
Esteban Bobbiesi <e.bobbiesi@gmail.com>
2023-04-21 02:28:29 -0300

    Renamed local.py settings file

---
+++ app/travel_blog/settings/dev.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+from .base import *
+
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+print(os.environ.get("DEBUG"))
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
+}
+
+
+STATIC_URL = "static/"
+
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR.child("media")
