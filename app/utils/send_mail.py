095ca85b6c154a226b93e253fb26c351d39d12f4
Esteban Bobbiesi <e.bobbiesi@gmail.com>
2023-04-24 07:33:05 -0300

    Fixed lint error (line too long)

--- app/utils/send_mail.py
+++ app/utils/send_mail.py
@@ -1,11 +1,15 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 def send_email(**kwargs):
-    full_name = kwargs.get('full_name')
-    email = kwargs.get('email')
-    subject = 'Bienvenido a nuestra aplicación'
-    message = f'Hola {full_name}, \n\n Bienvenido a nuestra aplicación! Su cuenta ha sido creada correctamente.'
+    full_name = kwargs.get("full_name")
+    email = kwargs.get("email")
+    subject = "Bienvenido a nuestra aplicación"
+    message = f"Hola {full_name},\
+        \n\nBienvenido a nuestra aplicación! Su cuenta ha sido creada correctamente."
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email, ]
+    recipient_list = [
+        email,
+    ]
     send_mail(subject, message, email_from, recipient_list, fail_silently=False)
