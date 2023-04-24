0d7a653f2b2728ecc52505148972065775b0ca9f
Esteban Bobbiesi <e.bobbiesi@gmail.com>
2023-04-24 05:02:23 -0300

    Added user profile and tests

--- app/user/views.py
+++ app/user/views.py
@@ -5,6 +5,8 @@ Views for the user API.
 from rest_framework import generics
 from rest_framework.settings import api_settings
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Serializers
 from .serializers import UserSerializer, AuthTokenSerializer
@@ -21,3 +23,15 @@ class LoginView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    """User profile view."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
